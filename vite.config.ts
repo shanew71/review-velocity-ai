c253b29d035fe60008be79bdb7cd1ecbfd7027ec
shanew71 <shane@shanewotherspoon.net>
2025-11-24 16:41:32 +1300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, (process as any).cwd(), '');
   return {
     plugins: [react()],
     define: {
       'process.env': {
-        API_KEY: JSON.stringify(env.API_KEY || process.env.API_KEY)
+        API_KEY: JSON.stringify(env.API_KEY || process.env.API_KEY),
+        VITE_GOOGLE_MAPS_API_KEY: JSON.stringify(env.VITE_GOOGLE_MAPS_API_KEY || process.env.VITE_GOOGLE_MAPS_API_KEY)
       }
     }
   };
